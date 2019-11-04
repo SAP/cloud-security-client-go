b613ee33b6f4bb6fec4991d313a1e43022fe2160
Felix Blass <felix.blass@sap.com>
2019-11-04 17:19:05 +0100

    Add support for xsuaa environment parsing on Cloud Foundry

--- env/environment.go
+++ env/environment.go
@@ -1,1 +1,60 @@
 package env
+
+import (
+	"github.com/cloudfoundry-community/go-cfenv"
+	"log"
+	"os"
+	"strings"
+)
+
+// possible combinations:
+// xsuaa + cf
+// xsuaa + kubernetes
+// ias + cf
+// ias + kubernetes
+
+type CloudEnvironment interface {
+	getClientID() string
+	getClientSecret() string
+	getSbURL() string
+	Parse(credentials map[string]interface{})
+}
+
+//
+//type CloudEnvironment struct {
+//	platform   Platform
+//	authServer AuthServer
+//}
+//
+//type Platform string
+//type AuthServer string
+//
+//const (
+//	CLOUD_FOUNDRY Platform   = "CLOUD_FOUNDRY"
+//	KUBERNETES    Platform   = "KUBERNETES"
+//	XSUAA         AuthServer = "XSUAA"
+//	IAS           AuthServer = "IAS"
+//)
+
+func GetEnvironment() CloudEnvironment {
+	var config CloudEnvironment
+	if strings.TrimSpace(os.Getenv("VCAP_APPLICATION")) != "" {
+		// Cloud Foundry
+		appEnv, e := cfenv.Current()
+		if e != nil {
+			log.Fatal("Could not read cf env")
+		}
+
+		xsuaa, e := appEnv.Services.WithName("xsuaa")
+		if e != nil {
+			log.Fatal("No xsuaa instance bound to the application")
+		} else {
+			config = XsuaaConfig{}
+			config.Parse(xsuaa.Credentials)
+		}
+
+	} else {
+		// kubernetes (supposably, as no other platforms are known so far)
+	}
+	return config
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-cloud-security-integration
 
 go 1.13
+
+require github.com/cloudfoundry-community/go-cfenv v1.18.0
