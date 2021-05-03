130cf60cc9ed5f91db0993bb353f472181849bf0
Felix Blass <felix.blass@sap.com>
2021-05-03 09:58:26 +0200

    Remove duplicate doc from implementation
    Documentation is now only on interface level (visible to the user)

--- auth/token.go
+++ auth/token.go
@@ -37,7 +37,7 @@ type Token interface {
 	UserUUID() string                                     // UserUUID returns "user_uuid" claim, if it doesn't exist empty string is returned
 	GetClaimAsString(claim string) (string, error)        // GetClaimAsString returns a custom claim type asserted as string. Returns error if the claim is not available or not a string.
 	GetClaimAsStringSlice(claim string) ([]string, error) // GetClaimAsStringSlice returns a custom claim type asserted as string slice. The claim name is case sensitive. Returns error if the claim is not available or not an array
-	GetAllClaimsAsMap() map[string]interface{}            // GetAllClaimsAsMap returns all a map of all claims contained in the token. This includes any custom claims
+	GetAllClaimsAsMap() map[string]interface{}            // GetAllClaimsAsMap returns a map of all claims contained in the token. The claim name is case sensitive. Includes also custom claims
 	getJwtToken() jwt.Token
 }
 
@@ -63,72 +63,59 @@ func (t stdToken) TokenValue() string {
 	return t.encodedToken
 }
 
-// Audience returns "aud" claim, if it doesn't exist empty string is returned
 func (t stdToken) Audience() []string {
 	return t.jwtToken.Audience()
 }
 
-// Expiration returns "exp" claim, if it doesn't exist empty string is returned
 func (t stdToken) Expiration() time.Time {
 	return t.jwtToken.Expiration()
 }
 
-// IsExpired returns true, if 'exp' claim + leeway time of 1 minute is before current time
 func (t stdToken) IsExpired() bool {
 	return t.Expiration().Add(1 * time.Minute).Before(time.Now())
 }
 
-// IssuedAt returns "iat" claim, if it doesn't exist empty string is returned
 func (t stdToken) IssuedAt() time.Time {
 	return t.jwtToken.IssuedAt()
 }
 
-// Issuer returns "iss" claim, if it doesn't exist empty string is returned
 func (t stdToken) Issuer() string {
 	return t.jwtToken.Issuer()
 }
 
-// NotBefore returns "nbf" claim, if it doesn't exist empty string is returned
 func (t stdToken) NotBefore() time.Time {
 	return t.jwtToken.NotBefore()
 }
 
-// Subject returns "sub" claim, if it doesn't exist empty string is returned
 func (t stdToken) Subject() string {
 	return t.jwtToken.Subject()
 }
 
-// GivenName returns "given_name" claim, if it doesn't exist empty string is returned
 func (t stdToken) GivenName() string {
 	v, _ := t.GetClaimAsString(givenName)
 	return v
 }
 
-// FamilyName returns "family_name" claim, if it doesn't exist empty string is returned
 func (t stdToken) FamilyName() string {
 	v, _ := t.GetClaimAsString(familyName)
 	return v
 }
 
-// Email returns "email" claim, if it doesn't exist empty string is returned
 func (t stdToken) Email() string {
 	v, _ := t.GetClaimAsString(email)
 	return v
 }
 
-// ZoneID returns "zone_uuid" claim, if it doesn't exist empty string is returned
 func (t stdToken) ZoneID() string {
 	v, _ := t.GetClaimAsString(sapGlobalZoneID)
 	return v
 }
 
-// UserUUID returns "user_uuid" claim, if it doesn't exist empty string is returned
 func (t stdToken) UserUUID() string {
 	v, _ := t.GetClaimAsString(sapGlobalUserID)
 	return v
 }
 
-// GetClaimAsString returns a custom claim type asserted as string. Returns error if the claim is not available or not a string.
 func (t stdToken) GetClaimAsString(claim string) (string, error) {
 	value, exists := t.jwtToken.Get(claim)
 	if !exists {
@@ -141,7 +128,6 @@ func (t stdToken) GetClaimAsString(claim string) (string, error) {
 	return stringValue, nil
 }
 
-// GetClaimAsStringSlice returns a custom claim type asserted as string slice. The claim name is case sensitive. Returns error if the claim is not available or not an array
 func (t stdToken) GetClaimAsStringSlice(claim string) ([]string, error) {
 	value, exists := t.jwtToken.Get(claim)
 	if !exists {
@@ -154,13 +140,11 @@ func (t stdToken) GetClaimAsStringSlice(claim string) ([]string, error) {
 	return res, nil
 }
 
-// GetAllClaimsAsMap returns a map of all claims contained in the token. The claim name is case sensitive. Includes also custom claims
 func (t stdToken) GetAllClaimsAsMap() map[string]interface{} {
 	mapClaims, _ := t.jwtToken.AsMap(context.TODO()) // err can not really occur on jwt.Token
 	return mapClaims
 }
 
-// getJwtToken returns jwt.Token
 func (t stdToken) getJwtToken() jwt.Token {
 	return t.jwtToken
 }
