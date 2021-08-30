284aba2056dc6dbead83706b2a2bc919f6218df3
Nena Raab <nena.raab@sap.com>
2021-08-30 18:53:34 +0200

    fix reuse stuff

--- env/environment.go
+++ env/environment.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Cloud Security Client Go contributors
+// SPDX-FileCopyrightText: 2020-2021 SAP SE or an SAP affiliate company and Cloud Security Client Go contributors
 //
 // SPDX-License-Identifier: Apache-2.0
 
