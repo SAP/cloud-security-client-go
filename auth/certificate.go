69d49ab170d1438d97017d3ca474e260bcd71795
Nena Raab <nena.raab@sap.com>
2021-11-19 15:37:47 +0100

    introduce Certificate instance

---
+++ auth/certificate.go
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Cloud Security Client Go contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+package auth
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+)
+
+type Certificate struct {
+	x509Cert *x509.Certificate
+}
+
+// newCertificate parses the X509 client certificate string.
+// It supports DER and PEM formatted certificates.
+// Returns nil, if certString is empty string.
+// Returns error in case of parsing error.
+func newCertificate(certString string) (*Certificate, error) {
+	x509Cert, err := parseCertificate(certString)
+	if x509Cert != nil {
+		return &Certificate{
+			x509Cert: x509Cert,
+		}, nil
+	}
+	return nil, err
+}
+
+func (c *Certificate) getThumbprint() string {
+	thumbprintBytes := sha256.Sum256(c.x509Cert.Raw)
+	thumbprint := base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(thumbprintBytes[:])
+
+	return thumbprint
+}
+
+func parseCertificate(certString string) (*x509.Certificate, error) {
+	if certString == "" {
+		return nil, nil
+	}
+	const PEMIndicator string = "-----BEGIN"
+	decoded, err := base64.StdEncoding.DecodeString(certString)
+	if err != nil {
+		return nil, fmt.Errorf("cannot base64 decode certificate header: %w", err)
+	}
+	if bytes.HasPrefix(decoded, []byte(PEMIndicator)) { // in case of apache proxy
+		pemBlock, err := pem.Decode(decoded)
+		if pemBlock == nil {
+			return nil, fmt.Errorf("cannot decode PEM formatted certificate header: %v", err)
+		}
+		decoded = pemBlock.Bytes
+	}
+	cert, err := x509.ParseCertificate(decoded)
+	if err != nil {
+		return nil, err
+	}
+	return cert, nil
+}
---
+++ auth/certificate_test.go
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Cloud Security Client Go contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+package auth
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCertificate(t *testing.T) {
+	t.Run("newCertificate() returns nil when no certificate is given", func(t *testing.T) {
+		cert, err := newCertificate("")
+		assert.Nil(t, cert)
+		assert.Nil(t, err)
+	})
+
+	t.Run("newCertificate() fails when DER certificate is corrupt", func(t *testing.T) {
+		cert, err := newCertificate("abc123")
+		assert.Nil(t, cert)
+		assert.Contains(t, err.Error(), "cannot base64 decode certificate header:")
+	})
+
+	t.Run("newCertificate() fails when PEM certificate is corrupt", func(t *testing.T) {
+		cert, err := newCertificate("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUQxVENDQXIyZ0F3SUJBZ0lNSUxvRXNuTFFCdQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0t")
+		assert.Nil(t, cert)
+		assert.Contains(t, err.Error(), "cannot decode PEM formatted certificate header:")
+	})
+
+	t.Run("getThumbprint() for PEM formatted cert", func(t *testing.T) {
+		cert, _ := newCertificate(readCert(t, "x-forwarded-client-cert.txt", true))
+		assert.Equal(t, "fU-XoQlhMTpQsz9ArXl6zHIpMGuRO4ExLKdLRTc5VjM", cert.getThumbprint())
+	})
+
+	t.Run("getThumbprint() for DER formatted cert", func(t *testing.T) {
+		cert, _ := newCertificate(readCert(t, "x-forwarded-client-cert.txt", false))
+		assert.Equal(t, "fU-XoQlhMTpQsz9ArXl6zHIpMGuRO4ExLKdLRTc5VjM", cert.getThumbprint())
+	})
+}
