61cb9c8e69106fde10eb232091aa961dc50854ee
Nena Raab <nena.raab@sap.com>
2021-12-01 19:48:52 +0100

    add tokenflows support - client credentials

---
+++ tokenclient/tokenFlows.go
@@ -0,0 +1,192 @@
+package tokenclient
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"github.com/sap/cloud-security-client-go/auth"
+	"github.com/sap/cloud-security-client-go/env"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Options struct {
+	HTTPClient *http.Client // TODO Default: basic http.Client with a timeout of 15 seconds
+	TLSConfig  *tls.Config  // TODO Default:
+}
+
+// RequestOptions allows to configure the token request
+type RequestOptions struct {
+	// Context carries the request context like the deadline or other values that should be shared across API boundaries.
+	Context context.Context
+	// Request parameters that shall be overwritten or added to the payload
+	Params map[string]string
+}
+
+type TokenFlows struct {
+	identity *env.Identity
+	options  Options
+	tokenURI string
+}
+
+type ClientError struct {
+	msg string
+	err error
+}
+
+func (e *ClientError) Error() string {
+	if e.err == nil {
+		return e.msg
+	}
+	return fmt.Sprintf("%s: %s", e.msg, e.err.Error())
+}
+
+type tokenResponse struct {
+	Token string `json:"access_token"`
+}
+
+const (
+	tokenEndpoint              string = "/oauth2/token" //nolint:gosec
+	grantTypeParameter         string = "grant_type"
+	grantTypeClientCredentials string = "client_credentials"
+	clientIDParameter          string = "client_id"
+	clientSecretParameter      string = "client_secret"
+)
+
+// NewTokenFlows initializes token flows
+//
+// identity provides credentials and url to authenticate client with identity service
+// options specifies rest client and its tls config, both can be overwritten
+func NewTokenFlows(identity *env.Identity, options Options) (*TokenFlows, *ClientError) {
+	t := new(TokenFlows)
+	t.identity = identity
+	if options.HTTPClient == nil {
+		if options.TLSConfig == nil && identity.IsCertificateBased() {
+			defaultConfig, err := DefaultTLSConfig(identity)
+			if err != nil {
+				return nil, err
+			}
+			options.TLSConfig = defaultConfig
+		}
+		options.HTTPClient = DefaultHTTPClient(options.TLSConfig)
+	}
+	t.options = options
+	t.tokenURI = identity.GetURL() + tokenEndpoint
+	return t, nil
+}
+
+// ClientCredentials implements the client credentials flow (RFC 6749, section 4.4).
+// Clients obtain an access token outside of the context of a user.
+// It is used for non interactive applications (a CLI, a batch job, or for service-2-service communication) where the token is issued to the application itself,
+// instead of an end user for accessing resources without principal propagation.
+//
+// options allows to provide a request context and optionally additional request parameters
+func (t *TokenFlows) ClientCredentials(customerTenantHost string, options RequestOptions) (auth.Token, *ClientError) {
+	data := url.Values{}
+	data.Set(grantTypeParameter, grantTypeClientCredentials)
+	data.Set(clientIDParameter, t.identity.GetClientID())
+	if t.identity.GetClientSecret() != "" {
+		data.Set(clientSecretParameter, t.identity.GetClientSecret())
+	}
+	for name, value := range options.Params {
+		data.Set(name, value) // potentially overwrites data which was set before
+	}
+	targetURL, err := t.getURL(customerTenantHost)
+	if err != nil {
+		return nil, err
+	}
+	ctx := options.Context
+	if ctx == nil {
+		log.Printf("uses context.TODO as fallback, as no context is provided with RequestOptions")
+		ctx = context.TODO()
+	}
+	r, e := http.NewRequestWithContext(ctx, http.MethodPost, targetURL, strings.NewReader(data.Encode())) // URL-encoded payload
+	if e != nil {
+		return nil, &ClientError{"error performing client credentials flow", e}
+	}
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	tokenJSON, err := t.performRequest(r)
+	if err != nil {
+		return nil, err
+	}
+	var response tokenResponse
+	_ = json.Unmarshal(tokenJSON, &response)
+	token, e := auth.NewToken(response.Token)
+	if e != nil {
+		return nil, &ClientError{"error parsing requested client credential token", e}
+	}
+	return token, nil
+}
+
+func (t *TokenFlows) getURL(customerTenantHost string) (string, *ClientError) {
+	if customerTenantHost == "" {
+		return t.tokenURI, nil
+	}
+	customHost, err := url.Parse(customerTenantHost)
+	if err == nil && customHost.Host != "" {
+		return "https://" + customHost.Host + tokenEndpoint, nil
+	}
+	return "", &ClientError{"customer tenant host '" + customerTenantHost + "' can't be accepted", err}
+}
+
+func (t *TokenFlows) performRequest(r *http.Request) ([]byte, *ClientError) {
+	res, err := t.options.HTTPClient.Do(r)
+	if err != nil {
+		return nil, &ClientError{"request to " + r.URL.String() + " failed", err}
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, &ClientError{"request to " + r.URL.String() + " failed with status code " + res.Status, err}
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err == nil && body != nil && json.Valid(body) {
+		return body, nil
+	}
+	return nil, &ClientError{"request to " + r.URL.String() + " provides no valid json content", err}
+}
+
+// TODO does it need to be public - how to avoid duplication
+func DefaultTLSConfig(identity *env.Identity) (*tls.Config, *ClientError) {
+	certPEMBlock := []byte(identity.GetCertificate())
+	keyPEMBlock := []byte(identity.GetKey())
+
+	tlsCert, err := tls.X509KeyPair(certPEMBlock, keyPEMBlock)
+	if err != nil {
+		return nil, &ClientError{"error creating x509 key pair for DefaultTLSConfig", err}
+	}
+	tlsCertPool, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, &ClientError{"error setting up cert pool for DefaultTLSConfig", err}
+	}
+	ok := tlsCertPool.AppendCertsFromPEM(certPEMBlock)
+	if !ok {
+		return nil, &ClientError{"error adding certs to pool for DefaultTLSConfig", err}
+	}
+	tlsConfig := &tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		RootCAs:      tlsCertPool,
+		Certificates: []tls.Certificate{tlsCert},
+	}
+	return tlsConfig, nil
+}
+
+// TODO does it need to be public - how to avoid duplication
+func DefaultHTTPClient(tlsConfig *tls.Config) *http.Client {
+	client := &http.Client{
+		Timeout: time.Second * 10, // TODO check
+	}
+	if tlsConfig != nil {
+		client.Transport = &http.Transport{
+			TLSClientConfig:     tlsConfig,
+			MaxIdleConnsPerHost: 50, // TODO check
+		}
+	}
+	return client
+}
---
+++ tokenclient/tokenFlows_test.go
@@ -0,0 +1,180 @@
+package tokenclient
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/sap/cloud-security-client-go/auth"
+	"github.com/sap/cloud-security-client-go/env"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+//go:embed testdata/certificate.pem
+var certificate string
+
+//go:embed testdata/privateKey.pem
+var key string
+
+//go:embed testdata/privateRSAKey.pem
+var otherKey string
+
+var mTLSConfig = &env.Identity{
+	ClientID:    "09932670-9440-445d-be3e-432a97d7e2ef",
+	Certificate: certificate,
+	Key:         key,
+	URL:         "https://aoxk2addh.accounts400.ondemand.com", // TODO fake it
+}
+
+var clientSecretConfig = &env.Identity{
+	ClientID:     "09932670-9440-445d-be3e-432a97d7e2ef",
+	ClientSecret: "[the_CLIENT.secret:3[/abc",
+	URL:          "https://mySaaS.accounts400.ondemand.com",
+}
+
+func TestNewTokenFlows_setupDefaultHttpClient(t *testing.T) {
+	tokenFlows, err := NewTokenFlows(clientSecretConfig, Options{})
+	assert.Nil(t, err)
+	assert.NotNil(t, tokenFlows)
+	assert.NotNil(t, tokenFlows.options.HTTPClient)
+	assert.Nil(t, tokenFlows.options.TLSConfig)
+}
+
+func TestNewTokenFlows_setupDefaultHttpsClient(t *testing.T) {
+	tokenFlows, err := NewTokenFlows(mTLSConfig, Options{})
+	assert.Nil(t, err)
+	assert.NotNil(t, tokenFlows)
+	assert.NotNil(t, tokenFlows.options.HTTPClient)
+	assert.NotNil(t, tokenFlows.options.TLSConfig)
+}
+
+func TestDefaultTLSConfig_shouldFailIfKeyDoesNotMatch(t *testing.T) {
+	mTLSConfig.Certificate = otherKey
+	tLSConfig, err := DefaultTLSConfig(mTLSConfig)
+	assert.Nil(t, tLSConfig)
+	assert.Error(t, err)
+}
+
+func TestClientCredentialsTokenFlow_FailsWithTimeout(t *testing.T) {
+	server := setupNewTLSServer(tokenHandler)
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(&env.Identity{URL: server.URL}, Options{HTTPClient: server.Client()})
+
+	timeout, cancelFunc := context.WithTimeout(context.Background(), 0*time.Second)
+	defer cancelFunc()
+	_, err := tokenFlows.ClientCredentials("", RequestOptions{Context: timeout})
+	assertClientError(t, context.DeadlineExceeded.Error(), err)
+}
+
+func TestClientCredentialsTokenFlow_FailsNoData(t *testing.T) {
+	server := setupNewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(&env.Identity{URL: server.URL}, Options{HTTPClient: server.Client()})
+
+	_, err := tokenFlows.ClientCredentials("", RequestOptions{})
+	assertClientError(t, "provides no valid json content", err)
+}
+
+func TestClientCredentialsTokenFlow_FailsUnexpectedJson(t *testing.T) {
+	server := setupNewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("{\"a\":\"b\"}")) })) //nolint:errcheck
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(&env.Identity{URL: server.URL}, Options{HTTPClient: server.Client()})
+
+	_, err := tokenFlows.ClientCredentials("", RequestOptions{})
+	assertClientError(t, "error parsing requested client credential token", err)
+}
+
+func TestClientCredentialsTokenFlow_FailsUnexpectedToken(t *testing.T) {
+	server := setupNewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("{\"access_token\":\"abc\"}")) })) //nolint:errcheck
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(&env.Identity{URL: server.URL}, Options{HTTPClient: server.Client()})
+
+	_, err := tokenFlows.ClientCredentials("", RequestOptions{})
+	assertClientError(t, "error parsing requested client credential token", err)
+}
+
+func TestClientCredentialsTokenFlow_FailsWithUnauthenticated(t *testing.T) {
+	server := setupNewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(401)
+	}))
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(&env.Identity{URL: server.URL}, Options{HTTPClient: server.Client()})
+
+	_, err := tokenFlows.ClientCredentials("", RequestOptions{})
+	assertClientError(t, "failed with status code 401 Unauthorized", err)
+}
+
+func TestClientCredentialsTokenFlow_FailsWithInvalidCustomHost(t *testing.T) {
+	server := setupNewTLSServer(tokenHandler)
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(mTLSConfig, Options{HTTPClient: server.Client()})
+
+	_, err := tokenFlows.ClientCredentials("invalidhost", RequestOptions{})
+	assertClientError(t, "customer tenant host 'invalidhost' can't be accepted", err)
+}
+
+func TestClientCredentialsTokenFlow_FailsWithInvalidUrls(t *testing.T) {
+	server := setupNewTLSServer(tokenHandler)
+	defer server.Close()
+	clientSecretConfig.URL = "invalidhost"
+	tokenFlows, _ := NewTokenFlows(clientSecretConfig, Options{HTTPClient: server.Client()})
+
+	_, err := tokenFlows.ClientCredentials("", RequestOptions{})
+	assertClientError(t, "unsupported protocol scheme", err)
+}
+
+func TestClientCredentialsTokenFlow_Succeeds(t *testing.T) {
+	server := setupNewTLSServer(tokenHandler)
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(&env.Identity{URL: server.URL}, Options{HTTPClient: server.Client()})
+
+	token, err := tokenFlows.ClientCredentials("", RequestOptions{Params: map[string]string{
+		"client_id": "09932670-9440-445d-be3e-432a97d7e2ef",
+	}})
+	assertToken(t, "eyJhbGciOiJIUzI1NiJ9.e30.ZRrHA1JJJW8opsbCGfG_HACGpVUMN_a9IV7pAx_Zmeo", token, err)
+}
+
+func TestClientCredentialsTokenFlow_SucceedsWithCustomHost(t *testing.T) {
+	server := setupNewTLSServer(tokenHandler)
+	defer server.Close()
+	tokenFlows, _ := NewTokenFlows(mTLSConfig, Options{HTTPClient: server.Client()})
+
+	token, err := tokenFlows.ClientCredentials(server.URL, RequestOptions{})
+	assertToken(t, "eyJhbGciOiJIUzI1NiJ9.e30.ZRrHA1JJJW8opsbCGfG_HACGpVUMN_a9IV7pAx_Zmeo", token, err)
+}
+
+func setupNewTLSServer(f func(http.ResponseWriter,
+	*http.Request)) *httptest.Server {
+	r := mux.NewRouter()
+	r.HandleFunc("/oauth2/token", f).Methods(http.MethodPost).Headers("Content-Type", "application/x-www-form-urlencoded")
+	return httptest.NewTLSServer(r)
+}
+
+// tokenHandler is the http handler which serves the /oauth2/token endpoint.
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	_, _ = buf.ReadFrom(r.Body)
+	newStr := buf.String()
+	if newStr == "client_id=09932670-9440-445d-be3e-432a97d7e2ef&grant_type=client_credentials" {
+		payload, _ := json.Marshal(tokenResponse{
+			Token: "eyJhbGciOiJIUzI1NiJ9.e30.ZRrHA1JJJW8opsbCGfG_HACGpVUMN_a9IV7pAx_Zmeo",
+		})
+		_, _ = w.Write(payload)
+	}
+}
+
+func assertToken(t assert.TestingT, expectedToken string, actualToken auth.Token, actualError *ClientError) {
+	assert.Nil(t, actualError)
+	assert.NotNil(t, actualToken)
+	assert.Equal(t, expectedToken, actualToken.TokenValue())
+}
+
+func assertClientError(t assert.TestingT, expectedErrorMsg string, actualError *ClientError) {
+	assert.Contains(t, actualError.Error(), expectedErrorMsg)
+	assert.IsType(t, actualError, (*ClientError)(nil))
+}
