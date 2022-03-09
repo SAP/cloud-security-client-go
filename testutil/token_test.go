7c31dd4956c17df4bfaef8b1f1959e5d1419c9c7
Felix Blass <felix.blass@sap.com>
2022-03-09 13:01:19 +0100

    Rename var

--- testutil/token_test.go
+++ testutil/token_test.go
@@ -2,22 +2,20 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-package mocks
+package testutil
 
 import (
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/sap/cloud-security-client-go/testutil"
 )
 
 func TestNewTokenFromClaims(t *testing.T) {
 	userUUID := uuid.NewString()
 	m := map[string]interface{}{"user_uuid": userUUID}
-	claims, err := testutil.NewTokenFromClaims(m)
+	token, err := NewTokenFromClaims(m)
 	assert.NoError(t, err)
 
-	assert.Equal(t, userUUID, claims.UserUUID(), "UserUUID() got = %v, want %v", claims.UserUUID(), userUUID)
+	assert.Equal(t, userUUID, token.UserUUID(), "UserUUID() got = %v, want %v", token.UserUUID(), userUUID)
 }
